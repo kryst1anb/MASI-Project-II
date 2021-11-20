3bb896b5ec606fdd35b9f781ec232e789ec1d159
m7lewandowska <marfalewandowska@gmail.com>
2021-11-20 18:23:38 +0100

    Sign In view

---
+++ MiASI_Project2_MedicalSupportSystem/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiASI_Project2_MedicalSupportSystem
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void HaveAccount_label_MouseEnter(object sender, EventArgs e)
+        {
+            HaveAccount_label.Font = new Font(HaveAccount_label.Font, FontStyle.Underline);
+        }
+
+        private void HaveAccount_label_MouseLeave(object sender, EventArgs e)
+        {
+            HaveAccount_label.Font = new Font(HaveAccount_label.Font, FontStyle.Regular);
+        }
+    }
+}
