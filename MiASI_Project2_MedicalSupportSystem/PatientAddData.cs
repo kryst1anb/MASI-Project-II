dd025b168d649162658aa6c2d387d87aa5577b7b
m7lewandowska <marfalewandowska@gmail.com>
2021-11-22 11:53:13 +0100

    add patient add data view

---
+++ MiASI_Project2_MedicalSupportSystem/PatientAddData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiASI_Project2_MedicalSupportSystem
+{
+    public partial class PatientAddData : Form
+    {
+        public PatientAddData()
+        {
+            InitializeComponent();
+        }
+    }
+}
