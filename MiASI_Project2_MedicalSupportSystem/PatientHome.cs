95f528226125ddcc7b84b3462afdb212b0480acd
m7lewandowska <marfalewandowska@gmail.com>
2021-11-28 17:25:49 +0100

    patient home view appearance depending on the existence of the sample

--- MiASI_Project2_MedicalSupportSystem/PatientHome.cs
+++ MiASI_Project2_MedicalSupportSystem/PatientHome.cs
@@ -7,17 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace MiASI_Project2_MedicalSupportSystem
 {
     public partial class PatientHome : Form
     {
         public static string role;
+        public static string userLogin;
         public PatientHome()
         {
             InitializeComponent();
             loginName_LB.Text = Login.loginDisplay;
             role = Login.userRole;
+            userLogin = Login.loginDisplay;
+
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=false;";
+            SqlConnection cnn = new SqlConnection(connectionString);
+
+            try
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT * FROM Project2.dbo.Samples as s INNER JOIN Project2.dbo.Users as u ON s.UserID = u.UserID where u.UserLogin Like '{userLogin}'", cnn);
+                SqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    noMedData_LB.Visible = false;
+                    add_MedData_BTN.Visible = false;
+
+                    resultData_Panel.Visible = true;
+                    medData_Panel.Visible = true;
+                }
+                else
+                {
+                    noMedData_LB.Visible = true;
+                    add_MedData_BTN.Visible = true;
+
+                    resultData_Panel.Visible = false;
+                    medData_Panel.Visible = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+
+
         }
 
         private void loginName_LB_Click(object sender, EventArgs e)
